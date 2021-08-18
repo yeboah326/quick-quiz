588c581fd3b778ab22637615570ec2bfe0cb6b6a
yeboah326 <asanteg36@gmail.com>
2021-08-18 12:33:38 +0000

    Modify run file

--- quick_quiz_api/run.py
+++ quick_quiz_api/run.py
@@ -1,9 +1,4 @@
-import time
+from quick_quiz_api.api import app
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/time")
-def get_current_time():
-    return {"time":time.time()}
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
