af997a48adca8ff6d73481db1db8fe4d3b4b1414
yeboah326 <asanteg36@gmail.com>
2021-08-18 12:32:49 +0000

    Add config module

---
+++ quick_quiz_api/config/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.environ.get('MYSQL_USERNAME')}:{os.environ.get('MYSQL_PASSWORD')}@{os.environ.get('MYSQL_SERVER')}/{os.environ.get('MYSQL_DATABASE_NAME')}"
+
+
+class ProductionConfig(Config):
+    ENV = "production"
+    DEBUG = False
+    DEVELOPMENT = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEVELOPMENT = True
+    DEBUG = True
