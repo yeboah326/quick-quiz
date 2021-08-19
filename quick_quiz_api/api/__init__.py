08f99375803833da3261d829f509c223c30fc751
yeboah326 <asanteg36@gmail.com>
2021-08-19 11:30:23 +0000

    Register new blueprints

--- quick_quiz_api/api/__init__.py
+++ quick_quiz_api/api/__init__.py
@@ -12,7 +12,16 @@ app.config.from_object(config.DevelopmentConfig)
 
 # Database
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
+
+from quick_quiz_api.api.auth.controllers import auth
+from quick_quiz_api.api.student.controllers import student
+from quick_quiz_api.api.tutor.controllers import tutor
+
+app.register_blueprint(auth)
+app.register_blueprint(student)
+app.register_blueprint(tutor)
+
 
 @app.route("/time")
 def get_current_time():
