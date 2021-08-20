9d5e2ffd8b17bb0ce5da731f3f490635f82e9b68
yeboah326 <asanteg36@gmail.com>
2021-08-20 12:10:13 +0000

    Add tests for the signup and login endpoints

--- quick_quiz_api/tests/test_auth.py
+++ quick_quiz_api/tests/test_auth.py
@@ -1,4 +1,12 @@
-from quick_quiz_api.tests.utils import drop_database_data
+from quick_quiz_api.api.student.models import Student
+from quick_quiz_api.api.tutor.models import Tutor
+from quick_quiz_api.tests.utils import (
+    drop_database_data,
+    create_new_student,
+    create_new_tutor,
+    test_data as data,
+)
+
 
 def test_auth_hello(app, client):
     drop_database_data()
@@ -12,12 +20,7 @@ def test_auth_signup_student_pass(app, client):
     drop_database_data()
     response = client.post(
         "auth/signup",
-        json={
-            "name": "John Student",
-            "email": "jdoe@doemail.com",
-            "password": "123456",
-            "user_type": "student",
-        },
+        json=data["student"]["valid_signup"],
     )
 
     assert response.status_code == 200
@@ -29,12 +32,7 @@ def test_auth_signup_student_fail(app, client):
     drop_database_data()
     response = client.post(
         "auth/signup",
-        json={
-            "name": None,
-            "email": "jdoe@doemail.com",
-            "password": "123456",
-            "user_type": "student",
-        },
+        json=data["student"]["invalid_signup"],
     )
 
     assert response.status_code == 400
@@ -46,12 +44,7 @@ def test_auth_signup_tutor_pass(app, client):
     drop_database_data()
     response = client.post(
         "auth/signup",
-        json={
-            "name": "John Tutor",
-            "email": "jdoe@doemail.com",
-            "password": "123456",
-            "user_type": "tutor",
-        },
+        json=data["tutor"]["valid_signup"],
     )
 
     assert response.status_code == 200
@@ -63,13 +56,96 @@ def test_auth_signup_tutor_fail(app, client):
     drop_database_data()
     response = client.post(
         "auth/signup",
-        json={
-            "name": "John Tutor",
-            "email": None,
-            "password": "123456",
-            "user_type": "tutor",
-        },
+        json=data["tutor"]["invalid_signup"],
     )
 
     assert response.status_code == 400
     assert response.json == {"message": "Tutor signup failed"}
+
+
+def test_auth_login_student_pass(app, client):
+    drop_database_data()
+    
+    create_new_student(client)
+
+    student = Student.query.filter_by(email=data["student"]["valid_login"]["email"]).first()
+    response = client.post("auth/login", json=data["student"]["valid_login"])
+
+    assert "token" in response.json
+    assert response.status_code == 200
+    assert response.json["public_id"] == student.public_id
+
+
+def test_auth_login_student_fail_no_email(app, client):
+    drop_database_data()
+    
+    create_new_student(client)
+
+    response = client.post("auth/login", json=data["student"]["invalid_login_no_email"])
+
+    assert response.status_code == 400
+    assert response.json == {"message": "Student not found or data is invalid"}
+
+
+def test_auth_login_student_fail_wrong_password(app, client):
+    drop_database_data()
+    
+    create_new_student(client)
+
+    response = client.post("auth/login", json=data["student"]["invalid_login_wrong_password"])
+
+    assert response.status_code == 401
+    assert response.json == {"message": "Authorization failed"}
+
+
+def test_auth_login_student_fail_wrong_email(app, client):
+    drop_database_data()
+    create_new_student(client)
+
+    response = client.post("auth/login", json=data["student"]["invalid_login_wrong_email"])
+
+    assert response.status_code == 400
+    assert response.json == {"message": "Student not found or data is invalid"}
+
+def test_auth_login_tutor_pass(app,client):
+    drop_database_data()
+    
+    create_new_tutor(client)
+
+    tutor = Tutor.query.filter_by(email=data["tutor"]["valid_login"]["email"]).first()
+
+    response = client.post("auth/login", json=data["tutor"]["valid_login"])
+
+    assert "token" in response.json
+    assert response.status_code == 200
+    assert response.json["public_id"] == tutor.public_id
+
+def test_auth_login_tutor_fail_no_email(app,client):
+    drop_database_data()
+    
+    create_new_tutor(client)
+
+    response = client.post("auth/login", json=data["tutor"]["invalid_login_no_email"])
+
+    assert response.status_code == 400
+    assert response.json == {"message": "Tutor not found or data is invalid"}
+
+def test_auth_login_tutor_fail_wrong_password(app,client):
+    drop_database_data()
+    
+    create_new_tutor(client)
+
+    response = client.post("auth/login", json=data["tutor"]["invalid_login_wrong_password"])
+
+    assert response.status_code == 401
+    assert response.json == {"message": "Authorization failed"}
+
+def test_auth_login_tutor_fail_wrong_email(app,client):
+    drop_database_data()
+    
+    create_new_student(client)
+
+    response = client.post("auth/login", json=data["student"]["invalid_login_wrong_email"])
+
+    assert response.status_code == 400
+    assert response.json == {"message": "Student not found or data is invalid"}
