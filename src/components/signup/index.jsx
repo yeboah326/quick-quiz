b13a8248613ed2fdb913344991fceffd9edf7966
yeboah326 <asanteg36@gmail.com>
2021-08-24 17:48:33 +0000

    Export created components

--- src/components/signup/index.jsx
+++ src/components/signup/index.jsx
@@ -1,1 +1,4 @@
 export { default as SignUpChoice } from "./SignUpChoice";
+export { default as SignUp } from "./SignUp";
+export { default as StudentSignUp } from "./StudentSignUp";
+export { default as TutorSignUp} from "./TutorSignUp";
