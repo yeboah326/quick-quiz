084251d663565ad193943588ce4922b9e575ee88
yeboah326 <asanteg36@gmail.com>
2021-08-24 03:32:57 +0000

    Add separate components for tutor login and student login

---
+++ src/components/login/StudentLogin.jsx
@@ -0,0 +1,9 @@
+import { Login } from "./index";
+
+function StudentLogin(){
+    return(
+        <Login user_type={"student"}/>
+    );
+}
+
+export default StudentLogin;
---
+++ src/components/login/TutorLogin.jsx
@@ -0,0 +1,9 @@
+import { Login } from "./index";
+
+function TutorLogin(){
+    return(
+        <Login user_type={"tutor"}/>
+    );
+}
+
+export default TutorLogin;
