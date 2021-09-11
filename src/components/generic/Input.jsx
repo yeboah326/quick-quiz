76cb717d941da738a7edd348102918eb05ce79b7
yeboah326 <asanteg36@gmail.com>
2021-09-11 04:42:29 +0000

    Modify existing components and import newly created ones

--- src/components/generic/Input.jsx
+++ src/components/generic/Input.jsx
@@ -1,10 +1,10 @@
 import {MontserratLightText} from "./index";
 
-function Input({type, className, Text}){
+function Input({type, className, Text, onChange}){
     return(
         <div style={{marginBottom:"1.5rem"}}>
             <MontserratLightText Text={Text} fontSize={1.3} />
-            <input type={type} className={className}/>
+            <input type={type} className={className} onChange={onChange}/>
         </div>
     );
 }
