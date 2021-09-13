a4201e283ccac6092d7d2fd894ba9e8dfdeb1650
yeboah326 <asanteg36@gmail.com>
2021-09-13 00:10:17 +0000

    Fixed signup and login functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import "../../styles/login/login.css";
 import { MontserratLightText, BreeText, Input, Button } from "../generic";
 import { IconmonstrAngelLeftCircleThin } from "../icons";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+// TODO: Add stock images to student and tutor login page
 
 function Login({ user_type }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Axios Setup
+  const instance = axios.create({
+    baseURL: "http://127.0.0.1:5000/",
+  });
+
+  const onSubmitClick = (event) => {
+    event.preventDefault();
+    instance({
+      method: "post",
+      url: "auth/login",
+      data: {
+        ...formData,
+        ...(user_type === "student" && { user_type: "student" }),
+        ...(user_type === "tutor" && { user_type: "tutor" }),
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .then(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handlePasswordChange = (event) =>
+    setFormData({ ...formData, password: event.target.value });
+  const handleEmailChange = (event) =>
+    setFormData({ ...formData, email: event.target.value });
+
   return (
     <div className="container">
       <div className="carousel">
@@ -30,20 +68,26 @@ function Login({ user_type }) {
           </div>
           <div className="form-fields">
             <div style={{ marginBottom: "1.5rem" }}>
-              <Input className="form-input" type={"text"} Text={"Email"} />
+              <Input
+                className="form-input"
+                type={"text"}
+                Text={"Email"}
+                onChange={handleEmailChange}
+              />
             </div>
             <div style={{ marginBottom: "1.5rem" }}>
               <Input
                 className="form-input"
                 type={"password"}
                 Text={"Password"}
+                onChange={handlePasswordChange}
               />
             </div>
             <div style={{ textAlign: "right" }}>
               <MontserratLightText Text={"Forgot password?"} fontSize={1} />
             </div>
             <div>
-              <Button Text={"Login"} />
+              <Button Text={"Login"} onClick={onSubmitClick} />
             </div>
           </div>
         </form>
