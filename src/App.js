e9b4f938dadb9740fb653e66113f3deb60ee7b7d
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-09-18 18:34:22 +0000

    Add route for page not found and added react toastify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import { LoginChoice, StudentLogin, TutorLogin } from "./components/login/";
 import { SignUpChoice, StudentSignUp, TutorSignUp } from "./components/signup/";
+import { PageNotFound } from "./components/generic";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div>
       <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Switch>
           <Route path="/login" exact component={LoginChoice} />
           <Route path="/login/tutor" exact component={TutorLogin} />
@@ -15,7 +28,8 @@ function App() {
           <Route path="/signup" exact component={SignUpChoice} />
           <Route path="/signup/tutor" exact component={TutorSignUp} />
           <Route path="/signup/student" exact component={StudentSignUp} />
-          <Route path="/dashboard" exact component={Dashboard}/>
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="*" exact component={PageNotFound}/>
         </Switch>
       </Router>
     </div>
